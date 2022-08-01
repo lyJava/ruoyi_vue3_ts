1fd65eb15a879b855cdee3337ec9850cc3eb2e72
Administrator <745876299@qq.com>
2022-08-01 23:47:10 +0800

    修复头像上传显示问题

--- ruoyi_ui_ts/src/views/system/user/profile/userAvatar.vue
+++ ruoyi_ui_ts/src/views/system/user/profile/userAvatar.vue
@@ -30,7 +30,8 @@
 				</el-col>
 				<el-col :xs="24" :md="12" :style="{ height: '350px' }">
 					<div class="avatar-upload-preview">
-						<img :src="previews.url" :style="previews.img" />
+						<!-- prettier-ignore -->
+						<img :src="options.previews.url" :style="options.previews.img" />
 					</div>
 				</el-col>
 			</el-row>
@@ -78,104 +79,95 @@
 					></el-button>
 				</el-col>
 				<el-col :lg="{ span: 2, offset: 6 }" :md="2">
-					<el-button type="primary" size="small" @click="uploadImg()"
-						>提 交</el-button
-					>
+					<!-- prettier-ignore -->
+					<el-button type="primary" size="small" @click="uploadImg()">提 交</el-button>
 				</el-col>
 			</el-row>
 		</el-dialog>
 	</div>
 </template>
 
-<script>
+<script lang="ts" setup>
 import useUserStore from "@/store/modules/user";
 import { uploadAvatar } from "@/api/system/user";
+import { ref, getCurrentInstance, reactive } from "vue";
 
 const baseURL = import.meta.env.VITE_APP_BASE_API;
 
-export default {
-	props: {
-		user: {
-			type: Object
-		}
+const { proxy } = getCurrentInstance() as any;
+// 是否显示弹出层
+const open = ref<boolean>(false);
+// 是否显示cropper
+const visible = ref<boolean>(false);
+// 弹出层标题
+const title = ref<string>("修改头像");
+const options = reactive<any>({
+	img: useUserStore().avatar, //裁剪图片的地址
+	autoCrop: true, // 是否默认生成截图框
+	autoCropWidth: 200, // 默认生成截图框宽度
+	autoCropHeight: 200, // 默认生成截图框高度
+	fixedBox: true, // 固定截图框大小 不允许改变
+	previews: {
+		url: "",
+		img: "",
 	},
-	data() {
-		return {
-			// 是否显示弹出层
-			open: false,
-			// 是否显示cropper
-			visible: false,
-			// 弹出层标题
-			title: "修改头像",
-			options: {
-				img: useUserStore().avatar, //裁剪图片的地址
-				autoCrop: true, // 是否默认生成截图框
-				autoCropWidth: 200, // 默认生成截图框宽度
-				autoCropHeight: 200, // 默认生成截图框高度
-				fixedBox: true // 固定截图框大小 不允许改变
-			},
-			previews: {}
+});
+
+// 编辑头像
+const editCropper = () => {
+	open.value = true;
+};
+// 打开弹出层结束时的回调
+const modalOpened = () => {
+	visible.value = true;
+};
+// 覆盖默认的上传行为
+const requestUpload: any = () => {};
+// 向左旋转
+const rotateLeft = () => {
+	proxy.$refs.cropper.rotateLeft();
+};
+// 向右旋转
+const rotateRight = () => {
+	proxy.$refs.cropper.rotateRight();
+};
+// 图片缩放
+const changeScale = (num: any) => {
+	num = num || 1;
+	proxy.$refs.cropper.changeScale(num);
+};
+// 上传预处理
+const beforeUpload = (file: any) => {
+	if (file.type.indexOf("image/") == -1) {
+		// prettier-ignore
+		proxy.$modal.msgError("文件格式错误，请上传图片类型,如：JPG，PNG后缀的文件。");
+	} else {
+		const reader = new FileReader();
+		reader.readAsDataURL(file);
+		reader.onload = () => {
+			options.img = reader.result;
 		};
-	},
-	methods: {
-		// 编辑头像
-		editCropper() {
-			this.open = true;
-		},
-		// 打开弹出层结束时的回调
-		modalOpened() {
-			this.visible = true;
-		},
-		// 覆盖默认的上传行为
-		requestUpload() {},
-		// 向左旋转
-		rotateLeft() {
-			this.$refs.cropper.rotateLeft();
-		},
-		// 向右旋转
-		rotateRight() {
-			this.$refs.cropper.rotateRight();
-		},
-		// 图片缩放
-		changeScale(num) {
-			num = num || 1;
-			this.$refs.cropper.changeScale(num);
-		},
-		// 上传预处理
-		beforeUpload(file) {
-			if (file.type.indexOf("image/") == -1) {
-                // prettier-ignore
-				this.$modal.msgError("文件格式错误，请上传图片类型,如：JPG，PNG后缀的文件。");
-			} else {
-				const reader = new FileReader();
-				reader.readAsDataURL(file);
-				reader.onload = () => {
-					this.options.img = reader.result;
-				};
-			}
-		},
-		// 上传图片
-		uploadImg() {
-			this.$refs.cropper.getCropBlob(data => {
-				let formData = new FormData();
-				formData.append("avatarfile", data);
-				uploadAvatar(formData).then(response => {
-					if (response.code === 200) {
-                        this.open = false;
-                        // prettier-ignore
-                        this.options.img = baseURL + response.imgUrl;
-                        store.commit("SET_AVATAR", this.options.img);
-                        this.$modal.msgSuccess("修改成功");
-                        this.visible = false;
-                    }
-				});
-			});
-		},
-		// 实时预览
-		realTime(data) {
-			this.previews = data;
-		}
 	}
+};
+// 上传图片
+const uploadImg = () => {
+	proxy.$refs.cropper.getCropBlob((data: any) => {
+		let formData = new FormData();
+		formData.append("avatarfile", data);
+		uploadAvatar(formData).then((response: any) => {
+			if (response.code === 200) {
+				open.value = false;
+				options.img = baseURL + response.data;
+				useUserStore().avatar = options.img;
+				proxy.$modal.msgSuccess("修改成功");
+				visible.value = false;
+			}
+		});
+	});
+};
+// 实时预览
+const realTime = (data: any) => {
+	options.previews = data;
 };
 </script>
 <style scoped lang="scss">
