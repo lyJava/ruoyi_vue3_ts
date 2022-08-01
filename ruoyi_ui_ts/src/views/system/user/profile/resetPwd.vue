75e8347e91ffd3f3247de6552ff881d2b3fe4e5b
Administrator <745876299@qq.com>
2022-08-02 06:09:01 +0800

    改为vue3及ts的改动

--- ruoyi_ui_ts/src/views/system/user/profile/resetPwd.vue
+++ ruoyi_ui_ts/src/views/system/user/profile/resetPwd.vue
@@ -1,10 +1,11 @@
 <template>
-	<el-form ref="form" :model="user" :rules="rules" label-width="80px">
+	<el-form ref="formRef" :model="user" :rules="rules" label-width="80px">
 		<el-form-item label="旧密码" prop="oldPassword">
 			<el-input
 				v-model="user.oldPassword"
 				placeholder="请输入旧密码"
 				type="password"
+                show-password
 			/>
 		</el-form-item>
 		<el-form-item label="新密码" prop="newPassword">
@@ -12,6 +13,7 @@
 				v-model="user.newPassword"
 				placeholder="请输入新密码"
 				type="password"
+                show-password
 			/>
 		</el-form-item>
 		<el-form-item label="确认密码" prop="confirmPassword">
@@ -19,90 +21,98 @@
 				v-model="user.confirmPassword"
 				placeholder="请确认密码"
 				type="password"
+                show-password
 			/>
 		</el-form-item>
 		<el-form-item>
-			<el-button type="primary" size="small" @click="submit"
-				>保存</el-button
-			>
+			<!-- prettier-ignore -->
+			<el-button type="primary" size="small" @click="submit">保存</el-button>
+			<!-- prettier-ignore -->
 			<el-button type="danger" size="small" @click="close">关闭</el-button>
 		</el-form-item>
 	</el-form>
 </template>
 
-<script>
+<script lang="ts" setup>
+import { ref, getCurrentInstance, defineExpose } from "vue";
 import { updateUserPwd } from "@/api/system/user";
+import { ElForm } from "element-plus";
+import useTagsViewStore from "@/store/modules/tagsView";
 
-export default {
-	data() {
-		const equalToPassword = (rule, value, callback) => {
-			if (this.user.newPassword !== value) {
-				callback(new Error("两次输入的密码不一致"));
-			} else {
-				callback();
-			}
-		};
-		return {
-			test: "1test",
-			user: {
-				oldPassword: undefined,
-				newPassword: undefined,
-				confirmPassword: undefined
-			},
-			// 表单校验
-			rules: {
-				oldPassword: [
-					{
-						required: true,
-						message: "旧密码不能为空",
-						trigger: "blur"
-					}
-				],
-				newPassword: [
-					{
-						required: true,
-						message: "新密码不能为空",
-						trigger: "blur"
-					},
-					{
-						min: 6,
-						max: 20,
-						message: "长度在 6 到 20 个字符",
-						trigger: "blur"
-					}
-				],
-				confirmPassword: [
-					{
-						required: true,
-						message: "确认密码不能为空",
-						trigger: "blur"
-					},
-					{
-						required: true,
-						validator: equalToPassword,
-						trigger: "blur"
-					}
-				]
-			}
-		};
-	},
-	methods: {
-		submit() {
-			this.$refs["form"].validate(valid => {
-				if (valid) {
-                    // prettier-ignore
-					updateUserPwd(this.user.oldPassword, this.user.newPassword).then(response => {
-						if (response.code === 200) {
-							this.$modal.msgSuccess("修改成功");
-						}
-					});
-				}
-			});
-		},
-		close() {
-			this.$store.dispatch("tagsView/delView", this.$route);
-			this.$router.push({ path: "/index" });
-		}
+const { proxy } = getCurrentInstance() as any;
+const formRef = ref<InstanceType<typeof ElForm>>();
+const equalToPassword = (rule: any, value: any, callback: any) => {
+	if (proxy.user.newPassword !== value) {
+		callback(new Error("两次输入的密码不一致"));
+	} else {
+		callback();
 	}
 };
+const user = ref({
+	oldPassword: undefined,
+	newPassword: undefined,
+	confirmPassword: undefined,
+});
+// 表单校验
+const rules = ref({
+	oldPassword: [
+		{
+			required: true,
+			message: "旧密码不能为空",
+			trigger: "blur",
+		},
+	],
+	newPassword: [
+		{
+			required: true,
+			message: "新密码不能为空",
+			trigger: "blur",
+		},
+		{
+			min: 6,
+			max: 20,
+			message: "长度在 6 到 20 个字符",
+			trigger: "blur",
+		},
+	],
+	confirmPassword: [
+		{
+			required: true,
+			message: "确认密码不能为空",
+			trigger: "blur",
+		},
+		{
+			required: true,
+			validator: equalToPassword,
+			trigger: "blur",
+		},
+	],
+});
+
+const submit = () => {
+	formRef.value?.validate((valid: boolean) => {
+		if (valid) {
+			// prettier-ignore
+			updateUserPwd(user.value.oldPassword, user.value.newPassword).then((response: any) => {
+                if (response.code === 200) {
+                    proxy.$modal.msgSuccess("修改成功");
+                    proxy.resetForm(formRef);
+                }
+            });
+		}
+	});
+};
+const close = () => {
+	useTagsViewStore().delView(proxy.$route);
+	proxy.$router.push({ path: "/index" });
+};
+
+const formReset = () => {
+	formRef.value?.resetFields();
+};
+
+// 暴露方法
+defineExpose({
+	formReset,
+});
 </script>
--- ruoyi_ui_ts/src/views/system/user/profile/userInfo.vue
+++ ruoyi_ui_ts/src/views/system/user/profile/userInfo.vue
@@ -1,5 +1,5 @@
 <template>
-	<el-form ref="form" :model="user" :rules="rules" label-width="80px">
+	<el-form ref="formRef" :model="user" :rules="rules" label-width="80px">
 		<el-form-item label="用户昵称" prop="nickName">
 			<el-input v-model="user.nickName" />
 		</el-form-item>
@@ -16,77 +16,82 @@
 			</el-radio-group>
 		</el-form-item>
 		<el-form-item>
-			<el-button type="primary" size="small" @click="submit"
-				>保存</el-button
-			>
-			<el-button type="danger" size="small" @click="close">关闭</el-button>
+			<!-- prettier-ignore -->
+			<el-button type="primary" size="small" @click="submit()">保存</el-button>
+			<!-- prettier-ignore -->
+			<el-button type="danger" size="small" @click="close()">关闭</el-button>
 		</el-form-item>
 	</el-form>
 </template>
 
-<script>
+<script lang="ts">
 import { updateUserProfile } from "@/api/system/user";
-
+import { ElForm } from "element-plus";
+import { ref, getCurrentInstance } from "vue";
+import useTagsViewStore from "@/store/modules/tagsView";
 export default {
 	props: {
 		user: {
-			type: Object
-		}
+			type: Object,
+		},
 	},
-	data() {
-		return {
-			// 表单校验
-			rules: {
-				nickName: [
-					{
-						required: true,
-						message: "用户昵称不能为空",
-						trigger: "blur"
-					}
-				],
-				email: [
-					{
-						required: true,
-						message: "邮箱地址不能为空",
-						trigger: "blur"
-					},
-					{
-						type: "email",
-						message: "'请输入正确的邮箱地址",
-						trigger: ["blur", "change"]
-					}
-				],
-				phonenumber: [
-					{
-						required: true,
-						message: "手机号码不能为空",
-						trigger: "blur"
-					},
-					{
-						pattern: /^1[3|4|5|6|7|8|9][0-9]\d{8}$/,
-						message: "请输入正确的手机号码",
-						trigger: "blur"
-					}
-				]
-			}
-		};
-	},
-	methods: {
-		submit() {
-			this.$refs["form"].validate(valid => {
+	setup(props: any) {
+		const { proxy } = getCurrentInstance() as any;
+		const formRef = ref<InstanceType<typeof ElForm>>();
+		const rules = ref<any>({
+			nickName: [
+				{
+					required: true,
+					message: "用户昵称不能为空",
+					trigger: "blur",
+				},
+			],
+			email: [
+				{
+					required: true,
+					message: "邮箱地址不能为空",
+					trigger: "blur",
+				},
+				{
+					type: "email",
+					message: "'请输入正确的邮箱地址",
+					trigger: ["blur", "change"],
+				},
+			],
+			phonenumber: [
+				{
+					required: true,
+					message: "手机号码不能为空",
+					trigger: "blur",
+				},
+				{
+					pattern: /^1[3|4|5|6|7|8|9][0-9]\d{8}$/,
+					message: "请输入正确的手机号码",
+					trigger: "blur",
+				},
+			],
+		});
+
+		const submit = () => {
+			formRef.value?.validate((valid: boolean) => {
 				if (valid) {
-					updateUserProfile(this.user).then(response => {
+					updateUserProfile(props.user).then((response: any) => {
 						if (response.code === 200) {
-							this.$modal.msgSuccess("修改成功");
+							proxy.$modal.msgSuccess("修改成功");
 						}
 					});
 				}
 			});
-		},
-		close() {
-			this.$store.dispatch("tagsView/delView", this.$route);
-			this.$router.push({ path: "/index" });
-		}
-	}
+		};
+		const close = () => {
+			useTagsViewStore().delView(proxy.$route);
+			proxy.$router.push({ path: "/index" });
+		};
+
+		// prettier-ignore
+		return {
+			rules, formRef, submit, close,
+        };
+	},
 };
 </script>
