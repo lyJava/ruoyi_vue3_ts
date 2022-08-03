4a06bc57f07d9fa854f38bc25f0f39af497d1101
Administrator <745876299@qq.com>
2022-08-04 07:09:16 +0800

    修复缓存查看转换异常

--- ruoyi/ruoyi-admin/src/main/java/com/ruoyi/web/controller/monitor/CacheController.java
+++ ruoyi/ruoyi-admin/src/main/java/com/ruoyi/web/controller/monitor/CacheController.java
@@ -1,18 +1,20 @@
 package com.ruoyi.web.controller.monitor;
 
+import com.alibaba.fastjson2.JSON;
 import com.github.xiaoymin.knife4j.annotations.ApiSort;
 import com.ruoyi.common.constant.CacheConstants;
 import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.core.redis.RedisCache;
 import com.ruoyi.common.utils.StringUtils;
 import com.ruoyi.system.domain.SysCache;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.data.redis.connection.RedisServerCommands;
 import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -31,6 +33,9 @@ public class CacheController {
 
     @Resource
     RedisTemplate<String, String> redisTemplate;
+
+    @Resource
+    RedisCache redisCache;
 
     private final static List<SysCache> caches = new ArrayList<>();
 
@@ -62,7 +67,8 @@ public class CacheController {
         result.put("dbSize", dbSize);
 
         List<Map<String, String>> pieList = new ArrayList<>();
-        Objects.requireNonNull(commandStats).stringPropertyNames().forEach(key -> {
+        Assert.notNull(commandStats, "执行状态不能为空");
+        commandStats.stringPropertyNames().forEach(key -> {
             Map<String, String> data = new HashMap<>(2);
             String property = commandStats.getProperty(key);
             data.put("name", StringUtils.removeStart(key, "cmdstat_"));
@@ -89,7 +95,14 @@ public class CacheController {
     @PreAuthorize("@ss.hasPermi('monitor:cache:list')")
     @GetMapping(value = "/getValue/{cacheName}/{cacheKey}")
     public AjaxResult<SysCache> getCacheValue(@PathVariable String cacheName, @PathVariable String cacheKey) {
-        String cacheValue = redisTemplate.opsForValue().get(cacheKey);
+        // String cacheValue = redisTemplate.opsForValue().get(cacheKey);
+        // TODO 这里可能会出现对象转换到String异常
+        String cacheValue = null;
+        if (redisCache.getCacheObject(cacheKey) instanceof String) {
+            cacheValue = redisCache.getCacheObject(cacheKey);
+        } else if (redisCache.getCacheObject(cacheKey) != null) {
+            cacheValue = JSON.toJSONString(redisTemplate.opsForValue().get(cacheKey));
+        }
         SysCache sysCache = new SysCache(cacheName, cacheKey, cacheValue);
         return AjaxResult.success(sysCache);
     }
@@ -98,9 +111,8 @@ public class CacheController {
     @DeleteMapping(value = "/clearCacheName/{cacheName}")
     public AjaxResult<String> clearCacheName(@PathVariable String cacheName) {
         Collection<String> cacheKeys = redisTemplate.keys(cacheName + "*");
-        if (CollectionUtils.isNotEmpty(cacheKeys)) {
-            redisTemplate.delete(cacheKeys);
-        }
+        Assert.notNull(cacheKeys, "缓存key不能为空");
+        redisTemplate.delete(cacheKeys);
         return AjaxResult.success();
     }
 
@@ -115,9 +127,8 @@ public class CacheController {
     @DeleteMapping("/clearCacheAll")
     public AjaxResult<String> clearCacheAll() {
         Collection<String> cacheKeys = redisTemplate.keys("*");
-        if (CollectionUtils.isNotEmpty(cacheKeys)) {
-            redisTemplate.delete(cacheKeys);
-        }
+        Assert.notNull(cacheKeys, "缓存key不能为空");
+        redisTemplate.delete(cacheKeys);
         return AjaxResult.success();
     }
 
