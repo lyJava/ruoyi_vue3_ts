2fb4463d101d451e002e56a14bccd3902bba1721
Administrator <745876299@qq.com>
2022-08-03 20:48:12 +0800

    缓存信息

--- ruoyi/ruoyi-admin/src/main/java/com/ruoyi/web/controller/monitor/CacheController.java
+++ ruoyi/ruoyi-admin/src/main/java/com/ruoyi/web/controller/monitor/CacheController.java
@@ -1,18 +1,19 @@
 package com.ruoyi.web.controller.monitor;
 
 import com.github.xiaoymin.knife4j.annotations.ApiSort;
+import com.ruoyi.common.constant.CacheConstants;
 import com.ruoyi.common.core.domain.AjaxResult;
 import com.ruoyi.common.utils.StringUtils;
+import com.ruoyi.system.domain.SysCache;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.data.redis.connection.RedisServerCommands;
 import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.*;
@@ -23,13 +24,25 @@ import java.util.*;
  * @author ruoyi
  */
 @ApiSort(value = 500)
-@Api(tags = "用户信息控制器")
+@Api(tags = "缓存监控控制器")
 @RestController
-@RequestMapping(value = "/monitor", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/monitor/cache", produces = MediaType.APPLICATION_JSON_VALUE)
 public class CacheController {
 
     @Resource
     RedisTemplate<String, String> redisTemplate;
+
+    private final static List<SysCache> caches = new ArrayList<>();
+
+    static {
+        caches.add(new SysCache(CacheConstants.LOGIN_TOKEN_KEY, "用户信息"));
+        caches.add(new SysCache(CacheConstants.SYS_CONFIG_KEY, "配置信息"));
+        caches.add(new SysCache(CacheConstants.SYS_DICT_KEY, "数据字典"));
+        caches.add(new SysCache(CacheConstants.CAPTCHA_CODE_KEY, "验证码"));
+        caches.add(new SysCache(CacheConstants.REPEAT_SUBMIT_KEY, "防重提交"));
+        caches.add(new SysCache(CacheConstants.RATE_LIMIT_KEY, "限流处理"));
+        caches.add(new SysCache(CacheConstants.PWD_ERR_CNT_KEY, "密码错误次数"));
+    }
 
     /**
      * 缓存监控信息
@@ -38,7 +51,7 @@ public class CacheController {
      */
     @ApiOperation(value = "缓存监控信息")
     @PreAuthorize("@ss.hasPermi('monitor:cache:list')")
-    @GetMapping(value = "/cache")
+    @GetMapping
     public AjaxResult<Map<String, Object>> getInfo() {
         Properties info = (Properties) redisTemplate.execute((RedisCallback<Object>) RedisServerCommands::info);
         Properties commandStats = (Properties) redisTemplate.execute((RedisCallback<Object>) connection -> connection.info("commandstats"));
@@ -59,4 +72,53 @@ public class CacheController {
         result.put("commandStats", pieList);
         return AjaxResult.success(result);
     }
+
+    @PreAuthorize("@ss.hasPermi('monitor:cache:list')")
+    @GetMapping("/getNames")
+    public AjaxResult<List<SysCache>> cache() {
+        return AjaxResult.success(caches);
+    }
+
+    @PreAuthorize("@ss.hasPermi('monitor:cache:list')")
+    @GetMapping("/getKeys/{cacheName}")
+    public AjaxResult<Set<String>> getCacheKeys(@PathVariable String cacheName) {
+        Set<String> cacheKeys = redisTemplate.keys(cacheName + "*");
+        return AjaxResult.success(cacheKeys);
+    }
+
+    @PreAuthorize("@ss.hasPermi('monitor:cache:list')")
+    @GetMapping(value = "/getValue/{cacheName}/{cacheKey}")
+    public AjaxResult<SysCache> getCacheValue(@PathVariable String cacheName, @PathVariable String cacheKey) {
+        String cacheValue = redisTemplate.opsForValue().get(cacheKey);
+        SysCache sysCache = new SysCache(cacheName, cacheKey, cacheValue);
+        return AjaxResult.success(sysCache);
+    }
+
+    @PreAuthorize("@ss.hasPermi('monitor:cache:list')")
+    @DeleteMapping(value = "/clearCacheName/{cacheName}")
+    public AjaxResult<String> clearCacheName(@PathVariable String cacheName) {
+        Collection<String> cacheKeys = redisTemplate.keys(cacheName + "*");
+        if (CollectionUtils.isNotEmpty(cacheKeys)) {
+            redisTemplate.delete(cacheKeys);
+        }
+        return AjaxResult.success();
+    }
+
+    @PreAuthorize("@ss.hasPermi('monitor:cache:list')")
+    @DeleteMapping(value = "/clearCacheKey/{cacheKey}")
+    public AjaxResult<String> clearCacheKey(@PathVariable String cacheKey) {
+        redisTemplate.delete(cacheKey);
+        return AjaxResult.success();
+    }
+
+    @PreAuthorize("@ss.hasPermi('monitor:cache:list')")
+    @DeleteMapping("/clearCacheAll")
+    public AjaxResult<String> clearCacheAll() {
+        Collection<String> cacheKeys = redisTemplate.keys("*");
+        if (CollectionUtils.isNotEmpty(cacheKeys)) {
+            redisTemplate.delete(cacheKeys);
+        }
+        return AjaxResult.success();
+    }
+
 }
