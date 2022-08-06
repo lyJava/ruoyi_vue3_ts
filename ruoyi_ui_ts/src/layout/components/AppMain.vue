ec88ad8d6a9f5d8609e38e1bf68aa51b127bb440
Administrator <745876299@qq.com>
2022-08-07 03:30:54 +0800

    使用router

--- ruoyi_ui_ts/src/layout/components/AppMain.vue
+++ ruoyi_ui_ts/src/layout/components/AppMain.vue
@@ -4,7 +4,7 @@
 		<router-view v-slot="{ Component, route }">
 			<transition name="fade-transform" mode="out-in">
 				<keep-alive :include="cachedViews">
-					<component :is="Component" :key="$route.fullPath"/>
+					<component :is="Component" :key="route.fullPath"/>
 				</keep-alive>
 			</transition>
 		</router-view>
