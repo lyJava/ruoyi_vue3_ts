950c52aa323f640a8909efeb873a6a5ded3ce76c
菜哥 <745876299@qq.com>
2025-04-11 19:24:37 +0800

    兼容ts改动

--- ruoyi_ui_ts/src/components/SvgIcon/svgicon.ts
+++ ruoyi_ui_ts/src/components/SvgIcon/svgicon.ts
@@ -1,10 +1,19 @@
 import * as components from "@element-plus/icons-vue";
 
-export default {
+/* export default {
 	install: (app: any) => {
 		for (const key in components) {
-			const componentConfig = components[key];
+			const componentConfig = components[key as keyof typeof components];
 			app.component(componentConfig.name, componentConfig);
 		}
+	},
+};
+ */
+
+export default {
+	install: (app: any) => {
+		Object.entries(components).forEach(([, component]) => {
+			app.component(component.name, component);
+		});
 	},
 };
