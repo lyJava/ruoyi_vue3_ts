8061ff2690f30f3a7b81427f2efed05d0261d8d0
菜哥 <745876299@qq.com>
2025-04-12 12:45:57 +0800

    返回上传文件名删除多的斜杠

--- ruoyi/ruoyi-common/src/main/java/com/ruoyi/common/utils/file/FileUploadUtils.java
+++ ruoyi/ruoyi-common/src/main/java/com/ruoyi/common/utils/file/FileUploadUtils.java
@@ -230,7 +230,7 @@ public class FileUploadUtils {
     public static String getPathFileName(String uploadDir, String fileName) throws IOException {
         int dirLastIndex = RuoYiConfig.getProfile().length() + 1;
         String currentDir = StringUtils.substring(uploadDir, dirLastIndex);
-        return Constants.RESOURCE_PREFIX + "/" + currentDir + "/" + fileName;
+        return Constants.RESOURCE_PREFIX + currentDir + "/" + fileName;
     }
 
     /**
