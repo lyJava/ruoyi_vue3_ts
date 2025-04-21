4a66311e63f287b3ed3ddd6f41dac3670e047f78
菜哥 <745876299@qq.com>
2025-04-21 11:57:32 +0800

    优先执行查询下拉框请求

--- ruoyi_ui_ts/src/api/request/system/notice/notice.ts
+++ ruoyi_ui_ts/src/api/request/system/notice/notice.ts
@@ -205,15 +205,7 @@ export default () => {
             });
 	};
 
-	onMounted(() => {
-		getList();
-		/* proxy.getDicts("sys_notice_status").then((response: { data: any }) => {
-			statusOptions.value = response.data;
-		});
-		proxy.getDicts("sys_notice_type").then((response: { data: any }) => {
-			typeOptions.value = response.data;
-		}); */
-		// 修改后的并发执行代码
+	const loadSelectData = async ()=> {
 		Promise.all([proxy.getDicts("sys_notice_status"), proxy.getDicts("sys_notice_type")]).then(([statusResponse, typeResponse]) => {
 			if (statusResponse.code === 200) {
 				statusOptions.value = statusResponse.data;
@@ -224,6 +216,11 @@ export default () => {
 		}).catch(error => {
 			console.error("请求失败:", error);
 		});
+	};
+
+	onMounted(() => {
+		loadSelectData();
+		getList();
 	});
 
 	// prettier-ignore
