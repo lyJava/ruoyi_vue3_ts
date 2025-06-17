7240295e03153cce48edc4b87028a37fb1d0f045
菜哥 <745876299@qq.com>
2025-06-18 03:11:20 +0800

    refactor：时间列宽度改动

--- ruoyi_ui_ts/src/views/system/dict/index.vue
+++ ruoyi_ui_ts/src/views/system/dict/index.vue
@@ -200,16 +200,8 @@
 				prop="remark"
 				:show-overflow-tooltip="true"
 			/>
-			<el-table-column label="创建时间" align="center" prop="createTime" width="150">
-				<template #default="scope">
-					<span>{{ scope.row.createTime }}</span>
-				</template>
-			</el-table-column>
-			<el-table-column label="修改时间" align="center" prop="updateTime" width="150">
-				<template #default="scope">
-					<span>{{ scope.row.updateTime }}</span>
-				</template>
-			</el-table-column>
+			<el-table-column label="创建时间" align="center" prop="createTime" width="200" />
+			<el-table-column label="修改时间" align="center" prop="updateTime" width="200" />
 			<el-table-column
 				label="操作"
 				align="center"
